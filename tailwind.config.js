6f70f14baef731ef225c5c2866939d7c3866532c
Chad Rosser <rosserchad@gmail.com>
2024-09-13 08:39:20 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    'index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2C3E50',
+        secondary: '#E74C3C',
+        accent1: '#ECF0F1',
+        accent2: '#BDC3C7',
+      },
+    },
+  },
+  plugins: [],
+}
+
